0a092e92703c69dc7b6ba0608f9a83811526ead0
Alejandro Gonzalez <alejandro@plasticolab.com>
2023-03-29 22:32:16 -0500

    seteo de campos en api productos

--- app/Http/Controllers/Fidepuntos/Api/V1/ProductosFidepuntosController.php
+++ app/Http/Controllers/Fidepuntos/Api/V1/ProductosFidepuntosController.php
@@ -37,8 +37,36 @@ class ProductosFidepuntosController extends Controller
      */
     public function show($id)
     {
+        $productos = [];
         $productosFidepuntos = ProductosFidepuntos::where('compania_id', $id)->where('activo', 1)->get();
-        return $productosFidepuntos;
+        foreach ($productosFidepuntos as $key => $pf) {
+            $producto_tmp = array(
+                'id' => $pf->id,
+                'nombre_producto' => $pf->nombre_producto,
+                'codigo_producto' => $pf->codigo_producto,
+                'ean' => $pf->ean,
+                'presentacion_talla_tamano' => $pf->presentacion_talla_tamano,
+                'fabricante' => $pf->fabricante->nombre_fabricante,
+                'marca' => $pf->marca->nombre_marca,
+                'categoria' => $pf->categoria->nombre_categoria,
+                'compania' => $pf->compania->nombre_compania,
+                'inventario' => $pf->inventario,
+                'tipo' => $pf->tipo,
+                'objetivo' => $pf->objetivo,
+                'oferta' => $pf->oferta,
+                'precio_unitario' => $pf->precio_unitario,
+                'iva' => $pf->iva,
+                'impoconsumo' => $pf->impoconsumo,
+                'descuento_porcentaje' => $pf->descuento_porcentaje,
+                'descuento_valor' => $pf->descuento_valor,
+                'precio' => $pf->precio,
+                'precio_puntos' => $pf->precio_puntos,
+                'fidelizacion' => $pf->fidelizacion,
+                'media_id_principal' => $pf->mediaprincial->url,
+            );
+            array_push($productos, $producto_tmp);
+        }
+        return $productos;
     }
 
     /**
