762a7d9ff0bd0a696643ff0391851539c2789c7b
Alejandro Gonzalez <alejandro@plasticolab.com>
2023-04-03 12:01:35 -0500

    modificacion en llamado de profile

--- app/Http/Controllers/Fidepuntos/Api/V1/ClienteFidepuntosController.php
+++ app/Http/Controllers/Fidepuntos/Api/V1/ClienteFidepuntosController.php
@@ -43,47 +43,8 @@ class ClienteFidepuntosController extends Controller
      */
     public function show($id)
     {
-        $productosEcommerce = [];
-        $productosCanje = [];
-        $productosMixto = [];
         $clienteFidepunto = ClienteFidepuntos::where('identificacion', $id)->first();
         $productosFidepunto = ProductosFidepuntos::where('compania_id', $clienteFidepunto->compania_id)->where('activo', 1)->get();
-        foreach ($productosFidepunto as $key => $pf) {
-            $producto_tmp = array(
-                'id' => $pf->id,
-                'nombre_producto' => $pf->nombre_producto,
-                'codigo_producto' => $pf->codigo_producto,
-                'ean' => $pf->ean,
-                'presentacion_talla_tamano' => $pf->presentacion_talla_tamano,
-                'fabricante' => $pf->fabricante->nombre_fabricante,
-                'marca' => $pf->marca->nombre_marca,
-                'categoria' => $pf->categoria->nombre_categoria,
-                'compania' => $pf->compania->nombre_compania,
-                'inventario' => $pf->inventario,
-                'tipo' => $pf->tipo,
-                'objetivo' => $pf->objetivo,
-                'oferta' => $pf->oferta,
-                'precio_unitario' => $pf->precio_unitario,
-                'iva' => $pf->iva,
-                'impoconsumo' => $pf->impoconsumo,
-                'descuento_porcentaje' => $pf->descuento_porcentaje,
-                'descuento_valor' => $pf->descuento_valor,
-                'precio' => $pf->precio,
-                'precio_puntos' => $pf->precio_puntos,
-                'fidelizacion' => $pf->fidelizacion,
-                'media_id_principal' => $pf->mediaprincial->url,
-            );
-            if ($pf->objetivo == 'ecommerce') {
-                array_push($productosEcommerce, $producto_tmp);
-            }
-            if ($pf->objetivo == 'canje') {
-                array_push($productosCanje, $producto_tmp);
-            }
-            if ($pf->objetivo == 'mixto') {
-                array_push($productosMixto, $producto_tmp);
-            }
-        }
-        /* return new ClienteFidepuntosResource($clienteFidepunto); */
         return response()->json([
             "status" => 200,
             "content" => "Success",
@@ -100,9 +61,6 @@ class ClienteFidepuntosController extends Controller
             "ciudad" => $clienteFidepunto->ciudad,
             "barrio" => $clienteFidepunto->barrio,
             "compania" => $clienteFidepunto->compania->nombre_compania,
-            "productos_ecommerce" => $productosEcommerce,
-            "productos_canje" => $productosCanje,
-            "productos_mixto" => $productosMixto,
         ]);
     }
 
