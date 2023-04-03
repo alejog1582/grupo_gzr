4e691898eeb1c791811fe4b217829e0496d7148d
Alejandro Gonzalez <alejandro@plasticolab.com>
2023-04-03 11:28:57 -0500

    solucion de cors en backend

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
