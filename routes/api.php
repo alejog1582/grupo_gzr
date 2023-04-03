10c03b4332fd4065fcd29be6ed6aba64305651d7
Alejandro Gonzalez <alejandro@plasticolab.com>
2023-04-03 11:31:58 -0500

    correccion error cors

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
+header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
